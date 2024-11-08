7f05ed445d7665d62d2879d815171fcbc089fdec
Akarsh8T <akarsh8t@gmail.com>
2024-11-08 18:43:03 +0530

    blog src

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// src/App.js
+
+import React, { useState } from 'react';
+import './App.css';
+import BlogList from './components/BlogList';
+import HospitalInfo from './components/HospitalInfo';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header Section */}
+      <header className="header">
+        <h1>Latest Blogs</h1>
+      </header>
+
+      {/* Blog Section */}
+      <main>
+        <BlogList />
+        <HospitalInfo />
+      </main>
+    </div>
+  );
+}
+
+export default App;
